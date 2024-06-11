e0649909bcc53bcf36fbc7c3d77d8b1da01d7f96
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2024-06-11 19:29:37 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState,useEffect, useRef } from "react";
 import Navbar from "./components/Navbar"
 import Introduction from "./components/Introduction"
 import Footerbar from "./components/Footerbar"
+import Socials from "./components/Socials";
 
 function App() {
   return (
     <>
       <Navbar />
       <Introduction />
+      <Socials />
       <Footerbar />
     </>
   )
